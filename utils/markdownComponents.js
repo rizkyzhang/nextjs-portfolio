4e24eecd5f92927aa09b0938f24a9cd199720ed1
rizkyzhang <rizkyzhangdev@gmail.com>
2022-06-04 15:28:41 +0700

    feat+fix: code syntax highlighting & fix overflow

--- utils/markdownComponents.js
+++ utils/markdownComponents.js
@@ -9,6 +9,9 @@ import {
   UnorderedList,
 } from "@chakra-ui/react";
 
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+
 const markdownComponents = {
   h2: ({ children }) => (
     <Heading my={5} size="lg">
@@ -30,7 +33,31 @@ const markdownComponents = {
   ul: ({ children }) => <UnorderedList p={5}>{children}</UnorderedList>,
   li: ({ children }) => <ListItem>{children}</ListItem>,
   img: ({ src, alt }) => <Image src={src} alt={alt} />,
-  code: ({ children }) => <Code px={3}>{children}</Code>,
+  code: ({ node, inline, className, children, ...props }) => {
+    const match = /language-(\w+)/.exec(className || "");
+    return !inline && match ? (
+      <SyntaxHighlighter
+        style={atomDark}
+        language={match[1]}
+        showLineNumbers
+        {...props}
+      >
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    ) : (
+      <Code
+        backgroundColor="rgb(29, 31, 33)"
+        color="white"
+        borderRadius="0.3em"
+        paddingX="0.6em"
+        paddingY="0.3em"
+        className={className}
+        {...props}
+      >
+        {children}
+      </Code>
+    );
+  },
 };
 
 export default markdownComponents;
